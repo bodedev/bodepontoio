677cc7c9b6461e50a54bd2900733f52156470af7
Cícero Raupp Rolim <baboo@bode.io>
2026-04-14 22:40:59 -0300

    fix: Corrige local para salvar logins de usuários

--- bodepontoio/signals.py
+++ bodepontoio/signals.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from bodepontoio.utils.cleaners import get_client_ip
 
 
 @receiver(post_save, sender=get_user_model())
@@ -9,3 +12,14 @@ def create_user_auth(sender, instance, created, **kwargs):
 
     if created:
         UserAuth.objects.get_or_create(user=instance)
+
+
+@receiver(user_logged_in)
+def save_login_record(sender, user, request, **kwargs):
+    from bodepontoio.models import LoginRecord
+
+    client_ip = get_client_ip(request)
+    LoginRecord.objects.create(
+        user=user,
+        ip=client_ip,
+    )
